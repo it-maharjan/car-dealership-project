010b8a61a5ba0281f618a091d6ce27b5e6e98ed7
Sujan Maharjan <it.maharjan@gmail.com>
2024-09-02 22:33:03 -0400

    before module 4

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -81,6 +81,7 @@ def registration(request):
 
 #Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
+    
     if(state == "All"):
         endpoint = "/fetchDealers"
     else:
