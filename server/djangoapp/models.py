132a11d61beecd0c35215f29023431cdc09d0e91
it-maharjan <it.maharjan@gmail.com>
2024-09-05 19:17:57 -0400

    linit commit for python

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -34,7 +34,7 @@ class CarMake(models.Model):
 # - __str__ method to print a car make object
 
 
-class CarModel(models.Model):      
+class CarModel(models.Model):
     # Many-to-One relationship
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
